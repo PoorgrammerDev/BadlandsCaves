37a8c6e7270237b74e561ddf2c189fd8b6d81e10
Poorgrammer <58582458+PoorgrammerDev@users.noreply.github.com>
2022-12-05 18:17:27 -0800

    Updated Guide book

--- src/main/java/me/fullpotato/badlandscaves/Info/GuideBook.java
+++ src/main/java/me/fullpotato/badlandscaves/Info/GuideBook.java
@@ -80,8 +80,26 @@ public class GuideBook {
         meta.spigot().addPage(getTOCEntries(entries_page_1, pages_page_1, false));
 
 
-        final String[] entries_page_2 = {"§4Hardmode§0", "§9Ascended Mobs"};
-        final String[] pages_page_2 = {"27", "29"};
+        final String[] entries_page_2 = {
+            "§4Hardmode§0",
+            "§9Ascended Mobs",
+            "§1Alternate Dimensions",
+            "§6Starlight Set",
+            "§3Nebulites",
+            "§5Voidmatter Set",
+            "§8Artifacts",
+        };
+
+        final String[] pages_page_2 = {
+            "27", //hardmode
+            "29", //asc mobs
+            "30", //alternate dims
+            "32", //starlight set
+            "34", //nebulites
+            "36", //vm
+            "38", //artifacts
+        };
+
         meta.spigot().addPage(getTOCEntries(entries_page_2, pages_page_2, true));
 
         meta.addPage("§lIntroduction§0\n" + "§0You awaken in a mysterious underground cave that seems to never stop. There are no signs of life around you, and all you have are tools to make a tree.");
@@ -96,7 +114,7 @@ public class GuideBook {
         meta.addPage("§3§lPuresoul§0\n" + "§0As a puresoul, you can invest into tech, starting with first a modified sword all the way to power armor, laser sabers, and laser guns in Hardmode. The tech-enhanced player is more durable than a magic user, but less agile.");
         meta.addPage("§3§lPuresoul (cont.)§0\n" + "§0You can modify swords to be Serrated, Voltshock, or Corrosive. Arrows can also be modified to be Voltshock or Corrosive. Serrated inflicts slow-acting DoT, Voltshock increases damage and stuns, and Corrosive inflicts fast-acting true DoT.");
         meta.addPage("§3§lPuresoul (cont.)§0\n" + "§0You can also craft shields from different materials like Cobblestone, Iron, and Diamond. These shields are more protective and durable than a regular shield, but also slow you down more.");
-        meta.addPage("§5§lSorcerer§0\n" + "§0A sorcerer uses black magic to weave between enemies and obstacles. Their powers work together to make a menacing force. The Sorcerer is less durable, but more agile.");
+        meta.addPage("§5§lSorcerer§0\n" + "§0A sorcerer uses black magic to weave between enemies and obstacles. Their powers work together to make them a menacing force. The Sorcerer is less durable, but more agile.");
         meta.addPage("§5§lSorcerer (cont.)§0\n" + "§0To access black magic, you must collect souls from mobs. Combine the souls of a zombie, zombified piglin, creeper, witch, ghast, silverfish, skeleton, spider, and phantom to make a Merged Soul.");
         meta.addPage("§5§lSorcerer (cont.)§0\n" + "§0Then, infuse that merged soul into a diamond. The result is a Soul Crystal, but it is incomplete. You must sacrifice one more soul - your own. Right-click to enter the World of Reflection and harvest your own soul.");
         meta.addPage("§5§lSorcerer (cont.)§0\n" + "§0After that, the Soul Crystal is complete. Again, right-click to enter Descension. Here, you must sneak past Lost Souls to capture four towers in a large ring. With every tower you capture, you gain more powers to use.");
@@ -109,7 +127,16 @@ public class GuideBook {
         meta.addPage("§4§lHardmode§0\n" + "§0After defeating The Wither for the first time, the world is irreversibly put into Hardmode. All mobs are significantly buffed. Thirst drains quicker, water doesn't quench as much, more materials are needed to purify, among other things.");
         meta.addPage("§4§lHardmode (cont.)§0\n" + "§0However, you now get access to crazier items, like Power Suits, Sabers, and Modules for the Puresouls, and Voidmatter Armor, Tools, and Artifacts for the Sorcerers.");
         meta.addPage("§9§lAscended Mobs§0\n" + "§0In Hardmode, there is a small chance of an Ascended Mob to spawn in the place of a regular mob. This chance is increased with Chaos. These mobs are VERY powerful and could be considered mini-bosses.");
-
+        meta.addPage("§1§lAlternate Dimensions§0\n" + "§0In Hardmode, breaking spawners now yields a new item: the Dimensional Anchor. Right clicking the anchor onto another spawner will open a portal to a different world.");
+        meta.addPage("§1§lAlternate Dimensions (cont.) §0\n" + "§0Each world has a set of environmental challenges to overcome, structures to find and explore, and a dangerous Void Layer to succumb to.");
+        meta.addPage("§6§lStarlight Set§0\n" + "§0The Starlight set is the set of end-game Hardmode power armor and tools for the Puresoul tech class. It offers immaculate protection, a reliable offense, and infinite reliability.");
+        meta.addPage("§6§lStarlight Set (cont.)§0\n" + "§0The set is crafted mainly via Titanium, Netherite, and Nether Stars. They must then be charged with Energy. If it runs out of charge, its functions will be limited, but it cannot break.");
+        meta.addPage("§3§lNebulites§0\n" + "§0The Starlight Set's functions can be further expanded with the use of Nebulites. These upgrade modules are found in structures in the depths of Alternate Dimensions.");
+        meta.addPage("§3§lNebulites (cont.)§0\n" + "§0They offer a variety of upgrades, from immunity to toxins to shooting through walls with the Blaster.");
+        meta.addPage("§5§lVoidmatter Set§0\n" + "§0The Voidmatter set is the set of end-game Hardmode armor and tools for the Sorcerer class. The set offers servicable protection, adequate offense, and massive potential via Artifacts.");
+        meta.addPage("§5§lVoidmatter Set (cont.)§0\n" + "§0The set is crafted via Voidmatter, which is dropped by enemies in the Void Layer of the Alternate Dimensions. The set can also be repaired in the crafting table with this very same Voidmatter.");
+        meta.addPage("§8§lArtifacts§0\n" + "§0Artifacts are metaphysical remnants of The Void that offer further expansion of abilities to a Sorcerer. Artifacts are found in structures in the depths of Alternate Dimensions, along with Nebulites.");
+        meta.addPage("§8§lArtifacts (cont.)§0\n" + "§0Artifacts are bound to the Sorcerer's soul via a Soul Campfire, and act upon their spells, Void Armor, and Void Tools. In many cases, they greatly expand on the original abilities.");
 
         book.setItemMeta(meta);
         return book;
